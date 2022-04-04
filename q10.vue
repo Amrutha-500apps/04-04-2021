d59cac33a9a221b4d8c1066da4ed35525aee9e18
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-04 16:53:55 +0530

    Add files via upload

---
+++ q10.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Form</h2>
+    <p>{{firstname}}</p>
+    <p>{{lastname}}</p>
+ </div>
+</template>
+<script>
+export default {
+    name:'hloA2', 
+    props:{
+      message:String,
+    },  
+    data(){
+        return{
+            firstname:"500apps",
+            lastname:"infinity",
+        }
+    },
+      methods: {
+        fullname(){
+            this.fullname = this.firstname + this.lastname;
+            console.log(this.fullname);
+        }
+    }  
+    
+}
+</script>
