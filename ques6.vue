51f7eded4d7a7c8d3b75fdd4c202a81f3a331825
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-06 19:11:11 +0530

    Add files via upload

---
+++ ques6.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h3>question 6</h3>
+        <p>{{name}}</p>
+    </div>    
+</template>
+<script>
+export default {
+    name:"childA3",
+    props:{
+        name1:String,
+    },
+    data(){
+        return{
+            name:"amrutha",
+        }
+    },
+    methods:{
+        func1(){
+            alert("ok"); 
+        }
+    }
+}
+</script>
