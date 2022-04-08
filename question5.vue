9411ce914b37c6f7d99388648c0701669f0aae37
Amrutha-500apps <102018614+Amrutha-500apps@users.noreply.github.com>
2022-04-08 18:05:07 +0530

    Add files via upload

---
+++ question5.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+<b-table striped hover :items="posts" :fields="fields"></b-table>
+{{posts}}
+</div>
+</template>
+<script>
+//var axios = require("axios").default;
+export default {
+   name:"quesSA",
+    data() {
+     return {
+     posts:' ',
+     fields:["web_pages","alpha_two_code","country","name","domains","state-province"],
+      }
+    },
+    methods: {
+    async getData() {
+    try {
+    let response = await fetch("http://universities.hipolabs.com/search?country");
+    this.posts = await response.json();
+     } catch (error) {
+    console.log(error);
+     }
+   },
+   },
+
+/*mounted() {
+
+axios
+
+.get("http://www.json-generator.com/api/json/get/bSldINskRK")
+
+.then(response=>(this.posts=response))
+
+}*/
+
+  created() { 
+  this.getData();
+  },
+}
+</script>
